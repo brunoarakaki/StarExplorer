9b3e20925ec1ceb1d8eff941fe51e31b4a3a0cba
Bruno Arakaki <brn.arakaki@gmail.com>
2023-07-02 19:14:30 -0500

    Add ship and ui

--- main.lua
+++ main.lua
@@ -14,44 +14,46 @@ math.randomseed(os.time())
 -- Configure image sheet
 local sheetOptions =
 {
-  frames=
-  { -- 1) asteroid 1
-    x=0,
-    y=0,
-    width=102,
-    height=85
-  },
+  frames =
   {
-    -- 2) asteroid 2
-    x=0,
-    y=85,
-    width=90,
-    height=83
-  },
-  {
-    -- 3) asteroid 3
-    x=0,
-    y=168,
-    width=100,
-    height=97
-  },
-  {
-    -- 4) ship
-    x=0,
-    y=265,
-    width=98,
-    height=79
-  },
-  {
-    -- 5) laser
-    x=98,
-    y=265,
-    width=14,
-    height=40
-  },
+    { -- 1) asteroid 1
+      x = 0,
+      y = 0,
+      width = 102,
+      height = 85
+    },
+    {
+      -- 2) asteroid 2
+      x = 0,
+      y = 85,
+      width = 90,
+      height = 83
+    },
+    {
+      -- 3) asteroid 3
+      x = 0,
+      y = 168,
+      width = 100,
+      height = 97
+    },
+    {
+      -- 4) ship
+      x = 0,
+      y = 265,
+      width = 98,
+      height = 79
+    },
+    {
+      -- 5) laser
+      x = 98,
+      y = 265,
+      width = 14,
+      height = 40
+    }
+  }
 }
 
-local objectSheet = graphics.newImageSheet("gameObjects.png", sheetOptions)
+local objectSheet = graphics.newImageSheet("gameObjects.png", sheetOptions )
 
 -- Initialize variables
 local lives = 3
@@ -75,3 +77,13 @@ local uiGroup = display.newGroup()    -- Display group for UI objects like the s
 local background = display.newImageRect(backGroup, "background.png", 800, 1400 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
+
+ship = display.newImageRect( mainGroup, objectSheet, 4, 98, 79 )
+ship.x = display.contentCenterX
+ship.y = display.contentHeight - 100
+physics.addBody( ship, { radius=30, isSensor=true } )
+ship.myName = "ship"
+
+-- Display lives and score
+livesText = display.newText(uiGroup, "Lives: " .. lives, 200, 80, native.systemFont, 36 )
+scoreText = display.newText(uiGroup, "Score: " .. score, 400, 80, native.systemFont, 36 )
