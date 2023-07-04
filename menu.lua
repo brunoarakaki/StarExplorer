356f530f748180462477252d3dbb0608d52c2d86
Bruno Arakaki <brn.arakaki@gmail.com>
2023-07-04 16:55:08 -0500

    Add menu buttons and title

--- menu.lua
+++ menu.lua
@@ -8,7 +8,13 @@ local scene = composer.newScene()
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
 
+local function goToGame()
+  composer.gotoScene( "game" )
+end
 
+local function goToHighscores()
+  composer.gotoScene( "highscores" )
+end
 
 
 -- -----------------------------------------------------------------------------------
@@ -21,6 +27,19 @@ function scene:create( event )
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 
+  local background = display.newImageRect( sceneGroup, "background.png", 800, 1400 )
+  background.x = display.contentCenterX
+  background.y = display.contentCenterY
+
+  local title = display.newImageRect( sceneGroup, "title.png", 500, 80 )
+  title.x = display.contentCenterX
+  title.y = 200
+
+  local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 700, native.systemFont, 44 )
+  playButton:setFillColor( 0.82, 0.86, 1 )
+
+  local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX, 810, native.systemFont, 44 )
+  highScoresButton:setFillColor( 0.75, 0.78, 1 )
 end
 
 
