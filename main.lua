a20b8d10eb9f4e61ca20b96856a2cb927092a105
Bruno Arakaki <brn.arakaki@gmail.com>
2023-07-04 23:30:54 -0500

    Reserve channel for background music

--- main.lua
+++ main.lua
@@ -11,3 +11,8 @@ display.setStatusBar( display.HiddenStatusBar )
 math.randomseed( os.time() )
 
 composer.gotoScene( "menu" )
+
+-- Reserve channel 1 for background music
+audio.reserveChannels( 1 )
+-- Reduce the overall volume of the channel
+audio.setVolume( 0.1, { channel=1 } )
